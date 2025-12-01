9b38648bd26969154b280a26ea6198de1bd433eb
umutkutlutr <umutkutlutr@gmail.com>
2025-12-01 19:26:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
+
+  import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     target: 'esnext',
-    outDir: 'dist', // Vercel burayı görmeli
+    outDir: 'build',
   },
   server: {
     port: 3000,
